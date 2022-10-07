09a1d2b5308197c9747471597be92e176731d922
Lukas Frey <mail@lukasfrey.cz>
2022-10-08 00:35:33 +0200

    ci: added semantic release workflow
    Signed-off-by: Lukas Frey <mail@lukasfrey.cz>

---
+++ release.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    "plugins": [
+        ["@semantic-release/commit-analyzer", {
+            "preset": "conventionalcommits",
+            "releaseRules": [
+                {"type": "docs", "scope":"README", "release": "patch"},
+                {"type": "refactor", "release": "patch"},
+                {"type": "chore", "release": "patch"},
+                {"type": "style", "release": "patch"},
+                {"type": "test", "release": "patch"},
+                {"type": "perf", "release": "patch"}
+            ],
+            "presetConfig": {
+                "types": [
+                    {"type": "feat", "section": "Features"},
+                    {"type": "fix", "section": "Bug Fixes"},
+                    {"type": "chore", "hidden": true},
+                    {"type": "docs", "hidden": true},
+                    {"type": "style", "hidden": true},
+                    {"type": "refactor", "hidden": true},
+                    {"type": "perf", "hidden": true},
+                    {"type": "test", "hidden": true}
+                ]
+            }
+        }],
+        ["@semantic-release/release-notes-generator", {
+            "preset": "conventionalcommits"
+        }],
+        // ["@semantic-release/github", {
+        //     "assets": [
+        //         {"path": "dist/plugin.css", "label": "CSS distribution"},
+        //     ]
+        // }],
+        // ["@semantic-release/changelog",
+        //     {
+        //         "changelogFile": "CHANGELOG.md"
+        //     }
+        // ],
+        // ["@semantic-release/git",
+        //     {
+        //         "assets": ["CHANGELOG.md"]
+        //     }
+        // ],
+        ["@semantic-release/npm", {
+            "tarballDir": "release",
+            "npmPublish": false
+        }],
+    ],
+    "tagFormat": "${version}"
+}
