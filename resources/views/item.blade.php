74977def6bead06b0fb758e8faadda1135bb83a7
Hugh Messenger <hugh.messenger@gmail.com>
2023-06-10 00:01:48 -0500

    Use @svg() directive rather than <x-icon> component to avoid clashes with other packages

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -1,10 +1,16 @@
 <div class="flex flex-col items-center justify-center">
-	<div class="relative w-full !h-16 flex flex-col items-center justify-center py-2">
-		<div class="relative w-12 h-12 grow-1 shrink-0 gap-1">
-			<x-icon name="{{$icon}}" class="w-full h-full absolute" />
-			{{-- Ugly fix for choices.js not registering clicks on SVGs. --}}
-			<div class="w-full h-full absolute z-10"></div>
-		</div>
-		<small class="w-full text-center grow-0 shrink-0 h-4 truncate">{{$icon}}</small>
-	</div>
+    <div class="relative flex !h-16 w-full flex-col items-center justify-center py-2">
+        <div class="grow-1 relative h-12 w-12 shrink-0 gap-1">
+            {{--
+                using x-icon clashes with wireui, tries to render their x-icon component }}
+                {{-- <x-icon name="{{$icon}}" class="w-full h-full absolute" />
+            --}}
+            @svg($icon, ['class' => 'w-full h-full absolute'])
+            {{-- Ugly fix for choices.js not registering clicks on SVGs. --}}
+            <div class="absolute z-10 h-full w-full"></div>
+        </div>
+        <small class="h-4 w-full shrink-0 grow-0 truncate text-center">
+            {{ $icon }}
+        </small>
+    </div>
 </div>
