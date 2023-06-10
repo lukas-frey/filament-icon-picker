78ead7731051feeb8b29513cbc4d79acf11cc235
Hugh Messenger <hugh.messenger@gmail.com>
2023-06-09 23:59:29 -0500

    Service provider changes for v3 (CSS assets)

--- src/FilamentIconPickerServiceProvider.php
+++ src/FilamentIconPickerServiceProvider.php
@@ -1,23 +1,28 @@
 <?php
 
 namespace Guava\FilamentIconPicker;
-use Filament\FilamentServiceProvider;
-//use Filament\Support\PluginServiceProvider;
+
+use Filament\Support\Assets\Css;
+use Filament\Support\Facades\FilamentAsset;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentIconPickerServiceProvider extends FilamentServiceProvider
+class FilamentIconPickerServiceProvider extends PackageServiceProvider
 {
-    protected array $styles = [
-        'filament-icon-picker-styles' => __DIR__ . '/../dist/plugin.css',
-    ];
-
     public function configurePackage(Package $package): void
     {
         $package
             ->name('filament-icon-picker')
             ->hasViews()
-            ->hasConfigFile('icon-picker')
-        ;
+            ->hasConfigFile('icon-picker');
     }
 
+    public function packageRegistered(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/icon-picker.php', 'filament-icon-picker');
+
+        FilamentAsset::register([
+            Css::make('icon-picker-stylesheet', __DIR__ . '/../resources/css/plugin.css'),
+        ], package: 'guava/filament-icon-picker');
+    }
 }
