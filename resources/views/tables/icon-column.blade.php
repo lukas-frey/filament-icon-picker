0d09a176b85064020b9b8baceec664d78eccc4d8
Hugh Messenger <hugh.messenger@gmail.com>
2023-06-12 13:22:54 -0500

    Only render if column has state

--- resources/views/tables/icon-column.blade.php
+++ resources/views/tables/icon-column.blade.php
@@ -1,4 +1,6 @@
 <div class="filament-icon-picker-icon-column px-4 py-3">
 {{--	<x-icon class="h-6" name="{{$getState()}}" />--}}
-	@svg($getState(), ['class' => 'h-6'])
+	@if($getState)
+        @svg($getState(), ['class' => 'h-6'])
+	@endif 
 </div>
