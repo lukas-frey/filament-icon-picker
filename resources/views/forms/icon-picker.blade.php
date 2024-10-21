7bf154376c654c5ec186bc662a8ad49848ab3af8
Abdulmejid Shemsu <abdulmejid@govassist.com>
2024-10-21 12:22:03 +0300

    fix: optimize CSS asset loading by using lazy loading

--- resources/views/forms/icon-picker.blade.php
+++ resources/views/forms/icon-picker.blade.php
@@ -1,5 +1,8 @@
 <div {{ $attributes->merge([
     'class' => "filament-icon-picker filament-icon-picker-{$getLayout()}",
-])->merge($getColumnsConfig()) }}>
+])->merge($getColumnsConfig()) }}
+x-data="{}"
+x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('filament-icon-picker-stylesheet', package: 'guava/filament-icon-picker'))]"
+>
 	@include('filament-forms::components.select')
 </div>
--- resources/views/tables/icon-column.blade.php
+++ resources/views/tables/icon-column.blade.php
@@ -1,4 +1,7 @@
-<div class="filament-icon-picker-icon-column px-4 py-3">
+<div class="filament-icon-picker-icon-column px-4 py-3"
+x-data="{}"
+x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('filament-icon-picker-stylesheet', package: 'guava/filament-icon-picker'))]"
+>
 	@if($icon = $getState())
 		<x-icon class="h-6" name="{{$icon}}" />
 	@endif
