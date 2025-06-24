01f67d1f965eb49ca5c0e92c4c2b6a21a007ddbf
Lukas Frey <mail@lukasfrey.cz>
2025-06-24 20:09:41 +0200

    refactor: cleanup Icon

--- src/Icons/Icon.php
+++ src/Icons/Icon.php
@@ -2,13 +2,8 @@
 
 namespace Guava\IconPickerPro\Icons;
 
-use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Support\Str;
-use function Filament\Support\generate_icon_html;
-
 class Icon
 {
-//    public string $html;
     public string $label;
 
     public function __construct(
@@ -16,12 +11,11 @@ class Icon
         public string $name,
         protected IconSet $set,
     ) {
-//        $this->html = generate_icon_html($this->id)->toHtml();
-        $this->label = str($this->name)->headline()->lower()->ucfirst();
+        $this->label = str($this->name)->afterLast('.')->headline()->lower()->ucfirst();
     }
 
-    public function getSet() {
+    public function getSet(): IconSet
+    {
         return $this->set;
     }
-
 }
